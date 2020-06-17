dfc6aabfc2c5a39283638ca421d5d69ce40b2455
kowalix007 L.K <kowalix007@gmail.com>
2020-06-17 21:08:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 require 'pry'
 def square_array(array)
   new_array=[]
-  array.each do |number| new_array<< number**2
+  array.each {|number| new_array<< number**2}
   return new_array
-  end
 # binding.pry
 end
